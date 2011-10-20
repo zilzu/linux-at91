450f53cfa4998bee14f7b7d3e496ae326f67f1f3
Hong Xu <hong.xu@atmel.com>
2011-10-20 14:33:31 +0800

    9n12: Add LED on AT91SAM9N12-EK
    Add LED on AT91SAM9N12-EK
    
    Signed-off-by: Hong Xu <hong.xu@atmel.com>

--- arch/arm/mach-at91/board-sam9n12ek.c
+++ arch/arm/mach-at91/board-sam9n12ek.c
@@ -355,25 +355,17 @@ static void __init ek_add_device_buttons(void) {}
  * LEDs ... these could all be PWM-driven, for variable brightness
  */
 static struct gpio_led ek_leds[] = {
-	{	/* "top" led, red, powerled */
+	{
 		.name			= "d8",
-		.gpio			= AT91_PIN_PD30,
+		.gpio			= AT91_PIN_PB4,
 		.default_trigger	= "heartbeat",
 	},
-	{	/* "left" led, green, userled2, pwm3 */
-		.name			= "d6",
-		.gpio			= AT91_PIN_PD0,
+	{
+		.name			= "d9",
+		.gpio			= AT91_PIN_PB5,
 		.active_low		= 1,
 		.default_trigger	= "nand-disk",
 	},
-#if !(defined(CONFIG_LEDS_ATMEL_PWM) || defined(CONFIG_LEDS_ATMEL_PWM_MODULE))
-	{	/* "right" led, green, userled1, pwm1 */
-		.name			= "d7",
-		.gpio			= AT91_PIN_PD31,
-		.active_low		= 1,
-		.default_trigger	= "mmc0",
-	},
-#endif
 };
 
 /*
