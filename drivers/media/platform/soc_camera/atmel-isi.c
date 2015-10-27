d5091415603153d508143cb75b3702cfed0018cb
Josh Wu <josh.wu@atmel.com>
2015-10-27 17:12:04 +0800

    media: atmel-isi: isc: add iscck support
    But iscck is optional.
    
    Signed-off-by: Josh Wu <josh.wu@atmel.com>

--- drivers/media/platform/soc_camera/atmel-isi.c
+++ drivers/media/platform/soc_camera/atmel-isi.c
@@ -99,6 +99,9 @@ struct atmel_isi {
 	struct completion		complete;
 	/* ISI peripherial clock */
 	struct clk			*pclk;
+	/* ISC clock */
+	struct clk			*iscck;
+
 	unsigned int			irq;
 
 	struct isi_platform_data	pdata;
@@ -1253,6 +1256,10 @@ static int atmel_isi_probe(struct platform_device *pdev)
 	if (IS_ERR(isi->pclk))
 		return PTR_ERR(isi->pclk);
 
+	isi->iscck = devm_clk_get(&pdev->dev, "iscck");
+	if (IS_ERR(isi->iscck))
+		isi->iscck = NULL;
+
 	ret = atmel_isi_parse_dt(isi, pdev);
 	if (ret)
 		return ret;
@@ -1355,6 +1362,8 @@ static int atmel_isi_runtime_suspend(struct device *dev)
 	struct atmel_isi *isi = container_of(soc_host,
 					struct atmel_isi, soc_host);
 
+	if (isi->iscck)
+		clk_disable_unprepare(isi->iscck);
 	clk_disable_unprepare(isi->pclk);
 
 	return 0;
@@ -1365,6 +1374,8 @@ static int atmel_isi_runtime_resume(struct device *dev)
 	struct atmel_isi *isi = container_of(soc_host,
 					struct atmel_isi, soc_host);
 
+	if (isi->iscck)
+		clk_prepare_enable(isi->iscck);
 	return clk_prepare_enable(isi->pclk);
 }
 #endif /* CONFIG_PM */
